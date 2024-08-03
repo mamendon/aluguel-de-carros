c44d78e6e8fc7111eff4689d2b79cc864910f19b
Marcelo Mendonça <mamendon93@gmail.com>
2024-08-03 11:53:32 -0300

    Commit

--- src/main/java/br/ufg/inf/bsi/alugueldecarros/dto/PessoaDTO.java
+++ src/main/java/br/ufg/inf/bsi/alugueldecarros/dto/PessoaDTO.java
@@ -7,6 +7,9 @@ public class PessoaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private Integer ID;
 	private String nome;
+	private String cpf;
+	private String email;
+	private String telefone;
 		
 	public PessoaDTO() {
 		super();
@@ -17,6 +20,7 @@ public class PessoaDTO implements Serializable{
 	public PessoaDTO(Pessoa obj) {
 		this.ID = obj.getId();
 		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
 	}
 
 
@@ -24,14 +28,35 @@ public class PessoaDTO implements Serializable{
 	public Integer getID() {
 		return ID;
 	}
-	public void setID(Integer iD) {
-		ID = iD;
+	public void setID(Integer id) {
+		ID = id;
 	}
 	public String getNome() {
 		return nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}	
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
 	}
 	
 	
